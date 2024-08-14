2d5c0824e51a2c8551023a0063e2396de6459fc1
SuperFlyz81 <109296884+SuperFlyz81@users.noreply.github.com>
2024-08-14 16:50:59 +0100

    Step 2 - Image stuff completed

--- src/components/Hero/HeroImage/HeroImage.jsx
+++ src/components/Hero/HeroImage/HeroImage.jsx
@@ -1,17 +1,17 @@
-
-import styles from './HeroImage.module.css'
-import next from 'next'
-import Image from next
-
+import styles from "./HeroImage.module.css";
+// import next from "next";
+import Image from "next/image";
 
 const HeroImage = () => {
-    return (
-        <Image
-        src="public\images\hero-desktop.png"
-        alt="Cozy living room with a fireplace"
-        className={styles.heroImage}
-      />
-    )
-}
+  return (
+    <Image
+      src="/images/hero-desktop.png"
+      alt="Cozy living room with a fireplace"
+      className={styles.heroImage}
+      width={500}
+      height={500}
+    />
+  );
+};
 
-export default HeroImage
+export default HeroImage;
