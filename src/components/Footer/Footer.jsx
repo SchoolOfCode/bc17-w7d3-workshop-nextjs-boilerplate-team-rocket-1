eb66bd80c28748c588d64b0a12603ae46096b607
SuperFlyz81 <109296884+SuperFlyz81@users.noreply.github.com>
2024-08-15 22:08:34 +0100

    Fixed Footer and HeroDescription width issues for Desktop responsive designs

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,33 +1,35 @@
-
-import styles from './Footer.module.css';
-import ExternalLink from './ExternalLink/ExternalLink';
-
+import styles from "./Footer.module.css";
+import ExternalLink from "./ExternalLink/ExternalLink";
 
 const Footer = () => {
-
-const socialLinks = [
-  {
-    href: 'https://www.facebook.com',
-    text: 'Facebook',
-  },
-  {
-    href: 'https://www.instagram.com',
-    text: 'Instagram',
-  },
-  {
-    href: 'https://www.tiktok.com',
-    text: 'TikTok',
-  },
-];
+  const socialLinks = [
+    {
+      href: "https://www.facebook.com",
+      text: "Facebook",
+    },
+    {
+      href: "https://www.instagram.com",
+      text: "Instagram",
+    },
+    {
+      href: "https://www.tiktok.com",
+      text: "TikTok",
+    },
+  ];
 
   return (
     <footer className={styles.footer}>
-      <div className={styles.colunmsContainer}>
+      <div className={styles.columnsContainer}>
         <div className={styles.footerColunm}>
           <p>Find us on:</p>
           <ul className={styles.footerNav}>
             {socialLinks.map((link, index) => (
-              <ExternalLink key={index} href={link.href} text={link.text} className={styles.socialLink} />
+              <ExternalLink
+                key={index}
+                href={link.href}
+                text={link.text}
+                className={styles.socialLink}
+              />
             ))}
           </ul>
         </div>
