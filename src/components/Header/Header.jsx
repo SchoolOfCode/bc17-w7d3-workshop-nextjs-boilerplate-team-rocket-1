8858884b3384377ea8b41534d47df9d20cfb2476
SuperFlyz81 <109296884+SuperFlyz81@users.noreply.github.com>
2024-08-15 11:27:32 +0100

    Step 4 completed - working on the menu functionality next

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,22 +1,25 @@
-'use client'
-import styles from './Header.module.css'
-import Link from 'next/link';
-import { useState } from 'react'
+"use client";
 
-const [ menuStatus, setMenuStatus ] = useState(false)
-
-function handleClick() {
-  setMenuStatus(!menuStatus)
-}
+import styles from "./Header.module.css";
+import Link from "next/link";
+import { useState } from "react";
 
 const Header = () => {
+  const [menuStatus, setMenuStatus] = useState(false);
+
+  function handleClick() {
+    setMenuStatus(!menuStatus);
+
+    console.log(menuStatus);
+  }
+
   return (
     <>
-    <header className={styles.headerContainer}>
-      <h1 className={styles.title}>🔥 Fireplace Palace</h1>
-      <button onClick={handleClick}>Racoon menu</button>
-      <Link href="/founders">Hamsa</Link>
-    </header>
+      <header className={styles.headerContainer}>
+        <h1 className={styles.title}>🔥 Fireplace Palace</h1>
+        <button onClick={handleClick}>Racoon menu</button>
+        <Link href="/founders">Hamsa</Link>
+      </header>
     </>
   );
 };
