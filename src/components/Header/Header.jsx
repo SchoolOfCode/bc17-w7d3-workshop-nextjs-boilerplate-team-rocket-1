44b7979a9cbbdbbffaaceb9f44443d2f4c687e3d
evamcconville <mcconvilleeva@gmail.com>
2024-08-15 11:01:28 +0100

    halfway through step 4

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,11 +1,20 @@
+'use client'
 import styles from './Header.module.css'
 import Link from 'next/link';
+import { useState } from 'react'
+
+const [ menuStatus, setMenuStatus ] = useState(false)
+
+function handleClick() {
+  setMenuStatus(!menuStatus)
+}
 
 const Header = () => {
   return (
     <>
     <header className={styles.headerContainer}>
       <h1 className={styles.title}>🔥 Fireplace Palace</h1>
+      <button onClick={handleClick}>Racoon menu</button>
       <Link href="/founders">Hamsa</Link>
     </header>
     </>
