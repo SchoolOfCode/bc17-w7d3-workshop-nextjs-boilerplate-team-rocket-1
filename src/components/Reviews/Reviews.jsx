b58854488bbd1f77ed5b2ec4354a17d412b58135
Hamsa <hamsamuse.hm@gmail.com>
2024-08-16 10:48:47 +0100

    stuff done

--- src/components/Reviews/Reviews.jsx
+++ src/components/Reviews/Reviews.jsx
@@ -4,11 +4,29 @@ import styles from "./Reviews.module.css";
 
 export default function Reviews() {
   const [selectedCountry, setSelectedCountry] = useState(null);
+  const [reviewData, setReviewData] = useState(null);
 
   function selectCountry(name) {
     console.log(name);
     setSelectedCountry(name);
   }
+
+  useEffect(() => {
+    if (selectedCountry) {
+      fetch(
+        `https://seal-app-336e8.ondigitalocean.app/reviews?country=scotland`)
+      .then(response => response.json())
+      .then(data=>{setReviewData(data);
+        console.log(data)
+      }) 
+     
+     
+    
+        
+        
+     
+    }
+  }, [selectedCountry]);
 
   return (
     <>
