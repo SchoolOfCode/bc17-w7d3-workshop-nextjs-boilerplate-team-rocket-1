5f94aca0b7327fc1e0c0136383392dbc086bc77c
evamcconville <mcconvilleeva@gmail.com>
2024-08-16 11:33:45 +0100

    halfway through step 6

--- src/components/Reviews/Reviews.jsx
+++ src/components/Reviews/Reviews.jsx
@@ -2,6 +2,10 @@
 import { useState, useEffect } from "react";
 import styles from "./Reviews.module.css";
 
+function Thingreview() {
+  
+}
+
 export default function Reviews() {
   const [selectedCountry, setSelectedCountry] = useState(null);
   const [reviewData, setReviewData] = useState(null);
@@ -14,25 +18,23 @@ export default function Reviews() {
   useEffect(() => {
     if (selectedCountry) {
       fetch(
-        `https://seal-app-336e8.ondigitalocean.app/reviews?country=scotland`)
+        `https://seal-app-336e8.ondigitalocean.app/reviews?country=${selectedCountry}`)
       .then(response => response.json())
       .then(data=>{setReviewData(data);
-        console.log(data)
       }) 
-     
-     
-    
-        
-        
-     
     }
   }, [selectedCountry]);
 
   return (
     <>
+    <article>
+    <h3>Trusted</h3>
+    <h4>We've got thousands  of happy customers all over the Uk. Choose your country to see the latest review</h4>
       <button onClick={() => selectCountry("England")}>England</button>
       <button onClick={() => selectCountry("Wales")}>Wales</button>
       <button onClick={() => selectCountry("Scotland")}>Scotland</button>
+      {selectedCountry && console.log(reviewData.author)}
+        </article>
     </>
-  );
+      );
 }
