fc23e63d531952e91a6f0137ee0359663c71229c
SuperFlyz81 <109296884+SuperFlyz81@users.noreply.github.com>
2024-08-20 11:07:51 +0100

    Design booking page created and linked to

---
+++ app/booking/page.js
@@ -0,0 +1,19 @@
+"use client";
+
+import { useState } from "react";
+import "./page.css";
+
+export default function ContactForm() {
+  return (
+    <>
+      <h1>Design Booking</h1>
+      <form>
+        <fieldset>
+          <legend>Choose your favorite monster</legend>
+
+          {/* Form labels and components (inputs) go here */}
+        </fieldset>
+      </form>
+    </>
+  );
+}
--- src/components/Hero/HeroDescription/HeroDescription.jsx
+++ src/components/Hero/HeroDescription/HeroDescription.jsx
@@ -1,18 +1,20 @@
-
-import styles from './HeroDescription.module.css'
+import styles from "./HeroDescription.module.css";
+import Link from "next/link";
 
 const HeroDescription = () => {
-    return (
-        <div className={styles.heroDescription}>
-        <p className={styles.heroHeadline}>
-          Discover the <br />
-          perfect fireplace ...
-        </p>
-        <p className={styles.consultationParagraph}>
-          Book consultation: <span className={styles.numberSpan}>0121 345 6789</span>
-        </p>
-      </div>
-    )
-}
+  return (
+    <div className={styles.heroDescription}>
+      <p className={styles.heroHeadline}>
+        Discover the <br />
+        perfect fireplace ...
+      </p>
+      <p className={styles.consultationParagraph}>
+        <Link className={styles.numberSpan} href="/booking">
+          Book consultation
+        </Link>
+      </p>
+    </div>
+  );
+};
 
-export default HeroDescription
+export default HeroDescription;
