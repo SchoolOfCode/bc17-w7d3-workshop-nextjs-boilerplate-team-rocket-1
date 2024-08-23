8c7c6897c30d19e1c137e6b0cba9669a712f1666
SuperFlyz81 <109296884+SuperFlyz81@users.noreply.github.com>
2024-08-23 11:34:00 +0100

    Test 1 and Test 2 work, halway through Test 3

--- tests/rocketReviewstest.test.js
+++ tests/rocketReviewstest.test.js
@@ -1,20 +1,43 @@
 import { test, expect } from "@playwright/test";
 
 // Test 1 - User Story 1: Regional Reviews
-test("team rocket test is the best", async ({ page }) => {
-  // Open our Fireplace Palace homepage
+test("User Story 1: Regional Reviews", async ({ page }) => {
   await page.goto("http://localhost:3000/");
 
-  // Check that we are on the correct website
   await expect(page).toHaveTitle(/Fireplace Palace/);
 
-  const menuButton = page.getByRole("button", { name: '≡' })
-  await menuButton.click()
-  const founderLink = page.getByRole('link', { name: 'Founders' })
-  expect(founderLink).toBeVisible()
-  await founderLink.click()
-  
+  const menuButton = page.getByRole("button", { name: "England" });
+  await menuButton.click();
+
+  const reviewText = page.getByText(
+    "Outstanding craftsmanship and attention to detail. Our living room has never felt so cozy. Thank you, Fireplace Palace!"
+  );
+  await expect(reviewText).toBeVisible();
+});
+
+// Test 2 - User Story 2: Founder's Page
+test("User Story 2: Founder's Page", async ({ page }) => {
+  await page.goto("http://localhost:3000/");
+
   await expect(page).toHaveTitle(/Fireplace Palace/);
-  
-  
+
+  const menuButton = page.getByRole("button", { name: "≡" });
+  await menuButton.click();
+  const founderLink = page.getByRole("link", { name: "Founders" });
+  expect(founderLink).toBeVisible();
+  await founderLink.click();
+
+  await expect(page).toHaveURL("http://localhost:3000/founders");
+});
+
+// Test 3 - User Story 3: Design Booking Page
+test("User Story 3: Design Booking Page", async ({ page }) => {
+  await page.goto("http://localhost:3000/");
+
+  await expect(page).toHaveTitle(/Fireplace Palace/);
+
+  const bookingLink = page.getByRole("link", { name: "Book consultation" });
+  await bookingLink.click();
+
+  //TODO - CARRY ON HERE
 });
