d604a1531d909e8767d04cfb5fcb9347a2964ede
Hamsa <hamsamuse.hm@gmail.com>
2024-08-23 11:57:53 +0100

    yesssssssssssssssssssssssssssssssss

--- tests/rocketReviewstest.test.js
+++ tests/rocketReviewstest.test.js
@@ -38,6 +38,40 @@ test("User Story 3: Design Booking Page", async ({ page }) => {
 
   const bookingLink = page.getByRole("link", { name: "Book consultation" });
   await bookingLink.click();
+  await expect(page).toHaveURL("http://localhost:3000/booking");
+  
+  const nameField = page.getByLabel('Name:');
+  await nameField.fill("Jessie")
+  expect(nameField).toHaveValue('Jessie')
+
+  const postcodeField = page.getByLabel('Postcode:')
+  await postcodeField.fill("UB32TJ")
+  expect(postcodeField).toHaveValue('UB32TJ')
+
+  
+  const addressField = page.getByLabel('Address:', { exact: true })
+  await addressField.fill("evil street")
+  expect(addressField).toHaveValue('evil street')
+
+  const cityField = page.getByLabel('City:')
+  await cityField.fill("vermilion")
+  expect(cityField).toHaveValue('vermilion')
+
+
+  const phoneNumberField = page.getByLabel( 'Phone Number:')
+  await phoneNumberField.fill('078723627632')
+  expect(phoneNumberField).toHaveValue('078723627632')
+ 
+  const bookingButton = page.getByRole('button', { name: 'BOOK NOW' })
+  await bookingButton.click()
+  const errorMessage = page.getByText(
+    "Fill in the form please!"
+  );
+  expect(errorMessage).toBeVisible()
+
+
+
+
 
   //TODO - CARRY ON HERE
 });
